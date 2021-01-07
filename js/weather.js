1603c0994ff9d2c72f47720e6578002959d70fe1
Henrique Bazi <68160678+henriquebazi@users.noreply.github.com>
2021-01-07 06:35:08 -0300

    Update weather.js

--- js/weather.js
+++ js/weather.js
@@ -1,5 +1,5 @@
 const APIKey = 'YU4IwyqAmR4tIT9jlvzZ4soLVoVv9vJs'
-const baseUrl = 'http://dataservice.accuweather.com/'
+const baseUrl = 'https://dataservice.accuweather.com/'
 
 const getCityUrl = city => 
 `${baseUrl}locations/v1/cities/search?apikey=${APIKey}&q=${city}`
